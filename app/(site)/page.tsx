5ac068b4fe160e18d55bd4ecf3bc45a4a186cae2
hrithik210 <hrithiknayak7829@gmail.com>
2024-10-21 14:57:29 +0530

    init

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-full flex flex-col justify-center
+    py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md ">
+        
+      </div>
+    </div>
+  );
+}
