cd839d6e804a86a9f6eb2c76e2fc8a8675386a08
hrithik210 <hrithiknayak7829@gmail.com>
2024-11-02 11:15:33 +0530

    Added userName show on the conversation page

--- components/UserBox.tsx
+++ components/UserBox.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { User } from "@prisma/client";
+import axios from "axios";
+
+import { useCallback, useState } from "react";
+import { Avatar } from "./Avatar";
+import { useRouter } from "next/navigation";
 
 interface UserBoxProps {
     data: User
@@ -8,8 +13,61 @@ interface UserBoxProps {
 const UserBox:React.FC<UserBoxProps> = ({
     data
 }) => {
+  const router = useRouter();
+  const [isLoading, setisLoading] = useState(false)
+
+
+  const handleClick = useCallback(() => {
+    setisLoading(true);
+
+    axios
+      .post("/api/conversation", {
+        userId: data.id,
+      })
+      .then((data) => {
+        router.push(`/conversation/${data.data.id}`);
+      })
+      .finally(() => setisLoading(false));
+  }, [data , router]);
+ 
   return (
-    <div>Users</div>
+    <div 
+      onClick = {handleClick}
+      className="
+        w-full
+        relative
+        flex
+        items-center
+        space-x-3
+        bg-white
+        p-3
+        hover:bg-neutral-100
+        rounded-lg
+        transition
+        cursor-pointer
+      ">
+        <Avatar user={data}/>
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-line">
+            <div className="
+            flex
+            justify-between
+            items-center
+            mb-1
+            ">
+            
+              <p className=" text-sm
+                  font-medium
+                  text-gray-900
+                  ">
+                    {data.name}
+              </p>
+
+            </div>
+          </div>
+        </div>
+
+    </div>
   )
 }
 
